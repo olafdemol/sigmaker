d7ad6c18598197d13c306c193c53ce2a49b0c195
Olaf de Mol <odmtalpa@gmail.com>
2025-04-12 14:27:29 +0200

    theming update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,22 +13,13 @@ import { theme } from "@/styles/theme";
 import { AppProvider } from "./provider";
 
 export const metadata = {
-  metadataBase: new URL("https://mantine-admin.vercel.app/"),
-  title: { default: "Mantine Admin", template: "%s | Mantine Admin" },
-  description: "A Modern Dashboard with Next.js.",
+  title: { default: "FloriSignature", template: "%s | FloriSignature" },
+  description: "FloriSignature is een tool waarmee je eenvoudig een e-mail handtekening kunt maken.",
   keywords: [
     "Next.js",
     "Mantine",
     "Signature"
   ],
-  authors: [
-    {
-      name: "jotyy",
-      url: "https://jotyy.vercel.app",
-    },
-  ],
-  creator: "jotyy",
-  manifest: "https://mantine-admin.vercel.app/site.webmanifest",
 };
 
 export default function RootLayout({
